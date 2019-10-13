61376b2a6c5c0879c7ca3c99bdcc2aa852637c6a
James H <jamesherrero@gmail.com>
2019-10-13 22:30:55 +1100

    Add method to pre-populate Note database
    NoteDatabase
      - Implement call back
      - Implement AsyncTask to insert new notes
      - Add callback to instance creation method

--- app/src/main/java/jherrero/architecture/example/NoteDatabase.java
+++ app/src/main/java/jherrero/architecture/example/NoteDatabase.java
@@ -1,10 +1,13 @@
 package jherrero.architecture.example;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 
 /*
@@ -33,9 +36,12 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()  // destroys database on new version
+                    .addCallback(roomCallback)  // when instance is created it will pre-populate our database
                     .build();
         }
         return instance;
+
+
 
          /*
          * Create single instance of database, which can be access. Should not be recreated
@@ -48,6 +54,36 @@ public abstract class NoteDatabase extends RoomDatabase {
          *  we will just use fallbackToDestructiveMigration(), which just re-installs database
          *  and restarts to version = 1
          * */
+    }
+
+    // Method is used so ass to prepopulate our database with data
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+
+        // Called the first time the database is created
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    // asynctask for database operations, thread should be running in a background thread
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {  // we don't pass anything to AsyncTask
+
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 1));
+            noteDao.insert(new Note("Title 3", "Description 3", 2));
+            noteDao.insert(new Note("Title 4", "Description 4", 3));
+            return null;
+        }
     }
 
 }
