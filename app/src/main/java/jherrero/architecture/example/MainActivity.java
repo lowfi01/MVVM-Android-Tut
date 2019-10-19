1ec1b60c21f9e934e4da9741e69d02bf86bc86c0
James H <jamesherrero@gmail.com>
2019-10-19 18:13:01 +1100

    Add AddNote form, submit to ROOM
    Created Add Note form, connect form to main activity
    using intent, then submit form data to ROOM
    
    Note - this implementation of using intents should be
    replaced with ViewModel, from my limited knownledge we
    can implement smaller ViewModels, or use fragments here
    
    ic_add, ic_close, ic_save
      - vectors used from the default android library
    
    add_note_menu.xml
      - custom menu for addNote activity
    
    AddNote.java
      - form for creating new note
      - use custom form by overrriding onCreateOptionMenu
      - add onclick listener for menu by overrriding onOptionItemSelected
      - submit data with saveNote()
          - saveNote should send to viewModel, not intent
    
    MainActivity.java
      - Floating button now interacts with AddNote.java activity
          - uses intents startActivityForResult()
          - implement constants
      - Submits new note using noteViewModel
          - Note - this logically should be done in AddNote.java
          But we are implementing a shortcut.
    
    AndroidManifest
      - Updated manifest to understand that back will go back button
      will go back to the main activity from addNote activity
    
          <activity android:name=".AddNote"
                android:parentActivityName=".MainActivity"/>
    
      - Updated manifest so that back button will not
      fire onCreate Method again, use singleTop
    
          <activity android:name=".MainActivity"
                android:launchMode="singleTop">

--- app/src/main/java/jherrero/architecture/example/MainActivity.java
+++ app/src/main/java/jherrero/architecture/example/MainActivity.java
@@ -1,19 +1,27 @@
 package jherrero.architecture.example;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ADD_NOTE_REQUEST = 1;
+
 
     private NoteViewModel noteViewModel;
+
 
 
     @Override
@@ -22,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        /*
+        *  Setup floating add action button which should move activities to
+        *  form and allow input, intent will carry form data.
+        *
+        *
+        *  Note - We are using Activities and intents to pass data & this is not
+        *  good practice, rather we should have a view model which will allow us to pass
+        *  data dynamically and not require the use of intents
+        *
+        * */
+        FloatingActionButton buttonAddNote = findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            // User has clicked add note button
+            @Override
+            public void onClick(View view) {
+                // do not use intents when setting up normally
+                Intent intent = new Intent(MainActivity.this, AddNote.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST); // send intent & wait
+
+            }
+        });
+
+
+        // Setup RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true); // only set when you know the size will not change, performance improvement
@@ -42,6 +74,42 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);  // adapter becomes a final variable
             }
         });
+
+    }
+
+    // Recieving intents sent cia startActivityForResult
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // form data sent successfully
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNote.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNote.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNote.EXTRA_PRIORITY, 1); // default value is to prevent null
+
+
+            // create new note with intent data from form submission
+            Note note = new Note(title,description, priority);
+
+
+            // insert note to database
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        } else {
+            // User has not saved the form, rather they have hit back button
+            // Note - this will not normally happen if you are using view model correctly
+            // rather we will need to find anther implementation
+
+
+            // note - RESULT_CANCELED is the resultCode send back
+
+
+            Toast.makeText(this, "Note Note Saved", Toast.LENGTH_SHORT).show();
+
+
+        }
 
 
     }
